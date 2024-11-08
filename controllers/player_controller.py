fb5b29125dcd5f57d05e5a08ebedef186f7c060b
Michael <56340359+SynysterRev@users.noreply.github.com>
2024-11-08 15:56:52 +0100

    start add players

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,4 +1,17 @@
+from deserializer import Deserializer
+from views.player_view import PlayerView
+
+
 class PlayerController:
+    def __init__(self):
+        self.view = PlayerView()
+
 
     def run(self):
-        pass
+        players_list = Deserializer.deserialize_players()
+        self.view.players_list = players_list
+        choice = self.view.show_menu()
+        if choice == 1:
+            print()
+        else:
+            return "main_menu"
--- views/player_view.py
+++ views/player_view.py
@@ -1,2 +1,21 @@
-class PlayerView:
-    pass
+from views.basic_view import BasicView
+
+
+class PlayerView(BasicView):
+    def __init__(self):
+        super().__init__()
+        self.name = "Joueurs"
+        self.players_list = []
+
+    def show_menu(self):
+        super(PlayerView, self).show_menu()
+        print("Joueurs enregistrés : ")
+        for player in self.players_list:
+            print(player)
+        print("1. Ajouter un nouveau joueur")
+        print("2. Revenir au menu principal")
+        return self.ask_for_user_choice(2)
+
+    def ask_new_player(self):
+        print()
+        # (bob, marlik, 10/18/2015) ()
