025feca949f531cb903357452908540d19cebb6d
Michael <56340359+SynysterRev@users.noreply.github.com>
2024-12-09 11:56:36 +0100

    remove comments

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -127,7 +127,6 @@ class TournamentController(BaseController):
                     continue
                 break
         self.accessible_menus = (Helper.get_new_tournament_menu(),
-                                 # Helper.get_modify_tournament_menu(),
                                  Helper.get_main_menu())
         self.max_selection = len(self.accessible_menus)
         self.view.accessible_menus = self.accessible_menus
@@ -234,28 +233,3 @@ class TournamentController(BaseController):
         elif self.current_menu == 2:
             searched_player_name = f"{self.searched_player.first_name} {self.searched_player.name}"
             self.view.render_validate_player(searched_player_name, self.current_selection)
-
-    # continue it if enough time
-    # def modify_tournament(self):
-    #     tournaments = Deserializer.deserialize_tournament()
-    #     self.view.display_all_tournaments(tournaments)
-    #     choice = self.view.ask_tournament_to_modify(len(tournaments))
-        # players = tournament.players
-        # players_encounters = {}
-        # if self.current_tournament.current_round == 1:
-        #     for player in players:
-        #         players_encounters[player] = []
-        #     random.shuffle(players)
-        #     matches = []
-        #     for i in range(0, len(self.current_tournament.players), 2):
-        #         match = Match(players[i], players[i + 1])
-        #         players_encounters[players[i]].append(players[i + 1])
-        #         players_encounters[players[i + 1]].append(players[i])
-        #         matches.append(match)
-        #
-        #     current_round = Round("Round 1", matches)
-        #     self.current_tournament.add_round(current_round)
-        # matches[0].set_winner(MatchResult.PLAYER_ONE)
-        # matches[0].update_player_one_score(1)
-        # matches[1].set_winner(MatchResult.PLAYER_TWO)
-        # matches[1].update_player_one_score(1)
